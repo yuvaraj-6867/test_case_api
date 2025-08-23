781c5c4558de239f32a699827da936a035142361
yuvaraj-6867 <yuvaraj6867@gmail.com>
2025-08-23 13:53:25 +0530

    Add root route and missing config files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+  def index
+    render json: { message: 'Test Case API is running', status: 'ok' }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'application#index'
+  
   namespace :api do
     namespace :v1 do
       resources :projects, only: [:index, :create, :show] do
