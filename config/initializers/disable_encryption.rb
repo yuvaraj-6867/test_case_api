fb9bae94b2b78bace8320d3913e1c9a47ca0c295
yuvaraj-6867 <yuvaraj6867@gmail.com>
2025-08-23 12:12:39 +0530

    Completely override ActiveRecord encryption module

--- config/initializers/disable_encryption.rb
+++ config/initializers/disable_encryption.rb
@@ -1,6 +1,15 @@
-Rails.application.configure do
-  # Disable ActiveRecord encryption completely
-  config.active_record.encryption.encrypt_fixtures = false
-  config.active_record.encryption.store_key_references = false
-  config.active_record.encryption.extend_queries = false
+# Completely disable ActiveRecord encryption to avoid credentials issues
+module ActiveRecord
+  module Encryption
+    def self.config
+      @config ||= OpenStruct.new(
+        encrypt_fixtures: false,
+        store_key_references: false,
+        extend_queries: false,
+        primary_key: nil,
+        deterministic_key: nil,
+        key_derivation_salt: nil
+      )
+    end
+  end
 end
